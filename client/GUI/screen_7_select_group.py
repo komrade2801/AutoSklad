810e2032303bae0433e05711d957b06d2d1b0fc8
Aleksandr Ushakov <sasha-ushakov@mail.ru>
2025-10-13 17:45:35 +0300

    added some debug logging

--- client/GUI/screen_7_select_group.py
+++ client/GUI/screen_7_select_group.py
@@ -1,14 +1,14 @@
 import traceback
 
-from PyQt5 import QtCore
 from PyQt5.QtWidgets import QListWidgetItem
 
-from DB.Models.Group import Group
 from .BaseScreen import BaseScreen
 from .ui_classes.Ui_screen_7_select_group import Ui_screen_7_select_group
+
 from PyQt5.QtCore import QEvent
 
 from .widgets.widget_select_group import WidgetSelectGroup
+from DB.Models.Group import Group
 
 
 class screen_7_select_group(BaseScreen, Ui_screen_7_select_group):
@@ -45,16 +45,22 @@ class screen_7_select_group(BaseScreen, Ui_screen_7_select_group):
         [{'id': 1, 'name': 'Group Name', 'description': 'Description', 'status': 0}, ...]
         """
         groups = args[0]
+        print(args[0])
         if not groups:
             return
+        print(groups)
         if not isinstance(groups, list):
             return
+        print(groups[0])
+        print(isinstance(groups[0], Group))
+        print(type(Group))
         if not isinstance(groups[0], Group):
             return
 
         self.listWidget.clear()  # Очищаем список перед добавлением новых данных
         try:
             for group in groups:
+                print(group)
 
                 if group.paren_group_id != 0:
                     continue
@@ -62,9 +68,11 @@ class screen_7_select_group(BaseScreen, Ui_screen_7_select_group):
                 widget = WidgetSelectGroup(self.trigger_name)
                 widget.set_data(group)  # Передаём данные в кастомный виджет
                 widget.event_select_group = self.handle_select_group
+                print(widget)
                 # widget.setSizeHint(QtCore.QSize(440, 80))  # Ширина и высота виджета
                 list_item = QListWidgetItem(self.listWidget)
                 list_item.setSizeHint(widget.sizeHint())  # Используем размер из виджета
+                print(list_item)
 
                 self.listWidget.addItem(list_item)
                 self.listWidget.setItemWidget(list_item, widget)
