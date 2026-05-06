5f4a70ce6dbb646fc1917c2adf459b1bfb629f3d
komrade2801 <komrade2801@gmail.com>
2026-05-06 12:51:33 +0300

    refactor(vending_startup): Remove font existence check and related logic from startup script to streamline initialization process

--- client/vending_startup.sh
+++ client/vending_startup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 # vending startup script
-
-FONT_FILE="/home/admin/.local/share/fonts/Google/TrueType/Roboto_Regular.ttf"
-if [ -f "$FILE" ]; then
-    echo "Font $FILE exists"
-else
-    echo "Font $FILE does not exist"
-    cp -r /home/admin/Desktop/client/GUI/assets/fonts /home/admin/.local/share/fonts
-fi
-
 cd /home/admin/Desktop/client
 source venv/bin/activate
 python3 main.py
